a012ef15fb447d9cf0048fcc2d320398a280b5e4
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-27 15:54:21 +0700

    Add okurigana splitter

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,52 @@
 import {analyze, MecabOptions} from "@enjoyjs/node-mecab";
 import * as wanakana from 'wanakana';
 
+
+function splitOkurigana(text: string, hiragana: string) {
+    const textPointer = [0, text.length - 1];
+    const kanaPointer = [0, hiragana.length - 1];
+    const direction = [1, -1];
+    const stored = [[], []];
+    for (let i = 0; i <= 1; i++) {
+        while (textPointer[0] <= textPointer[1]) {
+            if (!wanakana.isKanji(text[textPointer[i]])) {
+                stored[i].push({
+                    bottom: text[textPointer[i]],
+                    top: null
+                })
+                textPointer[i] += direction[i];
+                kanaPointer[i] += direction[i];
+            } else break;
+        }
+    }
+    stored[0].push({
+        bottom: text.substring(textPointer[0], textPointer[1] + 1),
+        top: text.substring(kanaPointer[0], kanaPointer[1] + 1)
+    })
+    stored[0].concat(stored[1].reverse())
+    return stored[0]
+}
+
 async function getFurigana(text: string, options?: Readonly<MecabOptions>) {
     const sentences = await analyze(text, options)
-    const words = [];
+    const pairs = [];
     for (const word of sentences.split('\n')) {
         if (word === 'EOS') continue;
         // console.log(word)
         const splittedWord = word.split('\t');
         const origin = splittedWord[0];
         const hiragana = wanakana.isKana(splittedWord[1]) ? wanakana.toHiragana(splittedWord[1], {passRomaji: true}) : splittedWord[1];
-        words.push({
+        const splitted = [];
+
+        pairs.push({
             origin, hiragana
         })
     }
-    return words
+    const ret = []
+    for (const wordPair of pairs) {
+        ret.push(splitOkurigana(wordPair.origin, wordPair.hiragana));
+    }
+    return ret
 }
 
 export {getFurigana};
