1f32e5ff5f1c772b4a27753cb1c99a8c311bf6d9
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-28 09:09:56 +0700

    New Package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {analyzeSync, MecabOptions} from "@enjoyjs/node-mecab";
 import * as wanakana from 'wanakana';
 
-
-function splitOkurigana(text: string, hiragana: string): any {
+function splitOkuriganaSparse(text: string, hiragana: string): any {
   const textPointer = [0, text.length - 1];
   const kanaPointer = [0, hiragana.length - 1];
   const direction = [1, -1];
@@ -12,8 +11,9 @@ function splitOkurigana(text: string, hiragana: string): any {
       if (!wanakana.isKanji(text[textPointer[i]])) {
         stored[i].push({
           main: text[textPointer[i]],
-          hiragana: null,
-          romaji: wanakana.toRomaji(text[textPointer[i]])
+          hiragana: wanakana.toHiragana(text[textPointer[i]]),
+          romaji: wanakana.toRomaji(text[textPointer[i]]),
+          isKana: true
         })
         textPointer[i] += direction[i];
         kanaPointer[i] += direction[i];
@@ -24,11 +24,55 @@ function splitOkurigana(text: string, hiragana: string): any {
     stored[0].push({
       main: text.substring(textPointer[0], textPointer[1] + 1),
       hiragana: hiragana.substring(kanaPointer[0], kanaPointer[1] + 1),
-      romaji: wanakana.toRomaji(hiragana.substring(kanaPointer[0], kanaPointer[1] + 1))
+      romaji: wanakana.toRomaji(hiragana.substring(kanaPointer[0], kanaPointer[1] + 1)),
+      isKana: false
     })
   }
   stored[0].concat(stored[1].reverse())
   return stored[0]
+}
+
+function splitOkuriganaCompact(text: string, hiragana: string): any {
+  const kanjiPointer = [text.length, -1];
+  const stored = []
+  for (let i = 0; i < text.length; i++) {
+    kanjiPointer[0] = i;
+    if (wanakana.isKanji(text[i])) break;
+  }
+  for (let i = text.length - 1; i >= 0; i--) {
+    kanjiPointer[1] = i;
+    if (wanakana.isKanji(text[i])) break;
+  }
+  if (kanjiPointer[0] > 0) {
+    const textHiragana = hiragana.substring(0, kanjiPointer[0]);
+    stored.push({
+      main: text.substring(0, kanjiPointer[0]),
+      hiragana: textHiragana,
+      romaji: wanakana.toRomaji(textHiragana),
+      isKana: true
+    })
+  }
+  if (kanjiPointer[0] <= kanjiPointer[1]) {
+    const spentBack = text.length - kanjiPointer[1];
+    const textHiragana = hiragana.substring(kanjiPointer[0], hiragana.length - spentBack + 1)
+    stored.push({
+      main: text.substring(kanjiPointer[0], kanjiPointer[1] + 1),
+      hiragana: textHiragana,
+      romaji: wanakana.toRomaji(textHiragana),
+      isKana: false
+    })
+  }
+  if (kanjiPointer[0] <= kanjiPointer[1] && kanjiPointer[1] + 1 !== text.length) {
+    const spentBack = text.length - kanjiPointer[1];
+    const textHiragana = hiragana.substring(hiragana.length - spentBack + 1, hiragana.length)
+    stored.push({
+      main: text.substring(kanjiPointer[1] + 1, text.length),
+      hiragana: textHiragana,
+      romaji: wanakana.toRomaji(textHiragana),
+      isKana: true
+    })
+  }
+  return stored;
 }
 
 function getFurigana(text: string, options?: Readonly<MecabOptions>) {
@@ -49,7 +93,7 @@ function getFurigana(text: string, options?: Readonly<MecabOptions>) {
   for (const wordPair of pairs) {
     ret.push({
       ...wordPair,
-      separation: splitOkurigana(wordPair.origin, wordPair.hiragana)
+      separation: splitOkuriganaCompact(wordPair.origin, wordPair.hiragana)
     });
   }
   return ret
