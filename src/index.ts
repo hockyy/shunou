e1b47febf7c7860b42b0e38b9dca382f2038080c
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-30 23:30:44 +0700

    Fix kanji checking

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,52 @@
 import * as wanakana from 'wanakana';
-import {isKana, isKanji, isMixed} from 'wanakana';
+import {isKana, isKanji, isMixed, toHiragana, toRomaji} from 'wanakana';
 import {spawnSync} from "child_process";
+
+function anyOneTrue(word: string, func) {
+  for (const ch of word) {
+    if (func(ch)) return true;
+  }
+  return false;
+}
+
+function createStoredObject(textMain: string, textHiragana: string) {
+  return {
+    main: textMain,
+    hiragana: textHiragana,
+    romaji: toRomaji(textHiragana),
+    isKana: anyOneTrue(textMain, isKana),
+    isKanji: anyOneTrue(textMain, isKanji),
+    isMixed: anyOneTrue(textMain, isMixed)
+  }
+}
 
 function splitOkuriganaCompact(text: string, hiragana: string): any {
   const kanjiPointer = [text.length, -1];
   const stored = []
   for (let i = 0; i < text.length + 1; i++) {
     kanjiPointer[0] = i;
-    if (i === text.length + 1 || wanakana.isKanji(text[i])) break;
+    if (i === text.length + 1 || isKanji(text[i])) break;
   }
   for (let i = text.length - 1; i >= 0; i--) {
     kanjiPointer[1] = i;
-    if (i === -1 || wanakana.isKanji(text[i])) break;
+    if (i === -1 || isKanji(text[i])) break;
   }
   if (kanjiPointer[0] > 0) {
     const textHiragana = hiragana.substring(0, kanjiPointer[0]);
     const textMain = text.substring(0, kanjiPointer[0])
-    stored.push({
-      main: textMain,
-      hiragana: textHiragana,
-      romaji: wanakana.toRomaji(textHiragana),
-      isKana: isKana(textMain),
-      isKanji: isKanji(textMain),
-      isMixed: isMixed(textMain)
-    })
+    stored.push(createStoredObject(textMain, textHiragana))
   }
   if (kanjiPointer[0] <= kanjiPointer[1]) {
     const spentBack = text.length - kanjiPointer[1];
     const textMain = text.substring(kanjiPointer[0], kanjiPointer[1] + 1)
     const textHiragana = hiragana.substring(kanjiPointer[0], hiragana.length - spentBack + 1)
-    stored.push({
-      main: textMain,
-      hiragana: textHiragana,
-      romaji: wanakana.toRomaji(textHiragana),
-      isKana: isKana(textMain),
-      isKanji: isKanji(textMain),
-      isMixed: isMixed(textMain)
-    })
+    stored.push(createStoredObject(textMain, textHiragana))
   }
   if (kanjiPointer[0] <= kanjiPointer[1] && kanjiPointer[1] + 1 !== text.length) {
     const spentBack = text.length - kanjiPointer[1];
     const textHiragana = hiragana.substring(hiragana.length - spentBack + 1, hiragana.length)
     const textMain = text.substring(kanjiPointer[1] + 1, text.length)
-    stored.push({
-      main: textMain,
-      hiragana: textHiragana,
-      romaji: wanakana.toRomaji(textHiragana),
-      isKana: isKana(textMain),
-      isKanji: isKanji(textMain),
-      isMixed: isMixed(textMain)
-    })
+    stored.push(createStoredObject(textMain, textHiragana))
   }
   return stored;
 }
@@ -61,7 +58,7 @@ function getFurigana(text: string, mecabCommand: string = 'mecab') {
     if (word === 'EOS') continue;
     const splittedWord = word.split('\t');
     const origin = splittedWord[0];
-    const hiragana = wanakana.isKana(splittedWord[1]) ? wanakana.toHiragana(splittedWord[1], {passRomaji: true}) : splittedWord[1];
+    const hiragana = isKana(splittedWord[1]) ? toHiragana(splittedWord[1], {passRomaji: true}) : splittedWord[1];
     const basicForm = splittedWord[3]
     const pos = splittedWord[4]
     pairs.push({
@@ -79,7 +76,7 @@ function getFurigana(text: string, mecabCommand: string = 'mecab') {
 }
 
 function isMixedJapanese(text: string): boolean {
-  return wanakana.isMixed(text)
+  return isMixed(text)
 }
 
 export {getFurigana, isMixedJapanese};
